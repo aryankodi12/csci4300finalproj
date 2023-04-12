6a52dc71114d4f562c5c8a79666d631598cc47de
mr-motley <mrm59255@uga.edu>
2023-04-12 14:33:05 -0400

    basic layout of page added

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -13,8 +13,17 @@ function App() {
   
   return (
     <div>
-      <div classname = "navBar">
+      <div className = "navBar">
         <NavBar user_inst={userLog}/>
+      </div>
+      <div className = "StatHeader">
+        <div className= "curveR"></div>
+        <h4>Player Name</h4>
+        <h4>Position</h4>
+        <h4>Height</h4>
+        <h4>Age</h4>
+        <h4>School</h4>
+        <div className= "curveL"></div>
       </div>
     </div>
   );
