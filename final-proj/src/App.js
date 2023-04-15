6cb1b335e2a7285ca32c9fe2915b0c2ad79debe1
JasonBajwa <jsb55449@uga.edu>
2023-04-14 20:26:59 -0400

    fix up App.js and included code for an error page

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -3,7 +3,7 @@ import Intro from './components/intro_page/Intro.js';
 import SignIn from './components/signin_page/SignIn';
 import CreateAccount from './components/create_account_page/CreateAccount';
 import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
-// import ErrorPage from
+import ErrorPage from './components/error_page/ErrorPage.js'
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
     <Route exact path='/' element={<Intro />}/>
     <Route exact path='/sign-in' element={<SignIn />}/>
     <Route exact path='/create-account' element={<CreateAccount />}/>
-    {/* <Route exact path='*' element={}/> */}
+    <Route exact path='*' element={<ErrorPage />}/>
   </Routes>
 </div>
 </Router>
---
+++ final-proj/src/components/error_page/ErrorPage.js
@@ -0,0 +1,9 @@
+import './ErrorPage.css';
+
+function ErrorPage () {
+    return (
+        <h1 className='title'>ERROR GO BACK</h1>
+    );
+}
+
+export default ErrorPage;
