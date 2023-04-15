db745500c58be47c1903537a9459d9e1e3295829
mr-motley <mrm59255@uga.edu>
2023-04-15 17:28:18 -0400

    fixed NavBar

--- final-proj/src/Components/home_page/NavBar.js
+++ final-proj/src/Components/home_page/NavBar.js
@@ -5,8 +5,11 @@ import './NavBar.css';
 function NavBar(props) {
     return(
         <div className ="wrap">
+            <div className="curve"></div> 
+            <div className="inner">
             <img className = "logo" src='https://i.imgur.com/xo4qwfm.png' alt="Roster Ready Logo"/>
             <Users user_list={props.user_inst}/>
+            </div>
         </div>
     );
 }
