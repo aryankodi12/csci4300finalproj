e09dfee73a2968978f56715ede9757fdd865c49f
mr-motley <mrm59255@uga.edu>
2023-04-16 13:08:32 -0400

    added logout feature

--- final-proj/src/Components/home_page/UserItem.js
+++ final-proj/src/Components/home_page/UserItem.js
@@ -1,12 +1,16 @@
 import './UserItem.css';
 import Card from './Card';
+import { Link } from 'react-router-dom';
 
 function UserItem(props){
 return (
     <div>
     <Card className='user-item'>
         <img src= {props.image} alt={props.name} className='user-class-img'/>
-        <h2 className='user-class-name'> {props.name}</h2>
+        <div className="uWrap">
+          <h2 className='user-class-name'> {props.name}</h2>
+          <Link to="/sign-in" className="log-out">Log Out</Link>
+        </div>
     </Card>
     </div>
   );
